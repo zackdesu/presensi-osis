534f55754f311b1dbc4225e856e67b5abf81ffb2
zackdesu <zarchxxx@gmail.com>
2023-07-18 07:50:28 +0700

    profile card for online and offline

--- frontend/src/components/profilecard.tsx
+++ frontend/src/components/profilecard.tsx
@@ -1,32 +1,33 @@
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { GoDotFill } from "react-icons/go";
+import { dataUser } from "./data";
 
 const Cards = ({
   img,
   name,
-  role,
+  status,
 }: {
   img: string;
   name: string;
-  role: string;
+  status: string;
 }) => (
-  <div className="mb-4 grid grid-cols-[.8fr_minmax(100px,_1.8fr)_.7fr] grid-rows-2 grid-flow-col">
-    <img
-      src={img}
-      className="row-span-2 self-center rounded-full"
-      width={30}
-      height={30}
-    />
+  <div className="mb-4 grid grid-cols-[.8fr_minmax(0px,_1.8fr)_.7fr] grid-rows-2 grid-flow-col">
+    <div className="sm:w-[30px] sm:h-[30px] w-[25px] h-[25px] row-span-2 self-center bg-cover">
+      <img
+        src={img}
+        className="rounded-full block object-cover w-full h-full"
+      />
+    </div>
     <p className="col-span-1 truncate ...">{name}</p>
     <p className="opacity-70 col-span-1 flex items-center truncate ...">
       <GoDotFill
         className={`mr-2 ${
-          role.toLowerCase() === "online" && "text-green-400"
+          status.toLowerCase() === "online" && "text-green-400"
         }`}
       />{" "}
-      {role}
+      {status}
     </p>
-    <BsThreeDotsVertical className="row-span-2 self-center justify-self-end" />
+    <BsThreeDotsVertical className="row-span-2 self-center justify-self-end cursor-pointer active:text-zinc-400" />
   </div>
 );
 
@@ -34,12 +35,19 @@ const ProfileCard = () => (
   <>
     <h4 className="mb-3">Pengurus</h4>
     <div className="overflow-y-auto w-full">
-      <Cards img="/unnamed.png" name="zackdesu" role="Online" />
-      <Cards img="/unnamed.png" name="zackdesu" role="Online" />
-      <Cards img="/unnamed.png" name="zackdesu" role="Online" />
-      <Cards img="/unnamed.png" name="zackdesu" role="Offline" />
-      <Cards img="/unnamed.png" name="zackdesu" role="Offline" />
-      <Cards img="/unnamed.png" name="zackdesu" role="Offline" />
+      {dataUser
+        .sort((a, b) => {
+          if (a.status === "Online" && b.status === "Offline") {
+            return -1;
+          } else if (a.status === "Offline" && b.status === "Online") {
+            return 1;
+          } else {
+            return 0;
+          }
+        })
+        .map((e, i) => (
+          <Cards name={e.name} img={e.img} status={e.status} key={i} />
+        ))}
     </div>
   </>
 );
