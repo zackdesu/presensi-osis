387ef12bed238d9f68d740709be65d4adbb40f14
zackdesu <zarchxxx@gmail.com>
2023-07-18 14:06:48 +0700

    gallery grid fix

--- frontend/src/pages/gallery.tsx
+++ frontend/src/pages/gallery.tsx
@@ -4,13 +4,26 @@ const Gallery = () => {
   return (
     <div className="grid grid-cols-4 md:grid-cols-12 grid-rows-6 h-full p-5 gap-8">
       <Header />
-      <div className="col-span-full row-span-6 grid grid-cols-4 md:grid-cols-12 grid-rows-6 h-full gap-8 overflow-y-auto">
-        <div className="col-span-full bg-black rounded-xl"></div>
-        <div className="col-span-2 row-span-2 bg-black rounded-xl"></div>
-        <div className="col-span-2 row-span-3 bg-black rounded-xl"></div>
-        <div className="col-span-2 row-span-2 bg-black rounded-xl"></div>
-        <div className="col-span-2 row-span-2 bg-black rounded-xl"></div>
-        <div className="col-span-2 row-span-1 bg-black rounded-xl"></div>
+      <div className="col-span-full row-span-5 grid grid-cols-4 md:grid-cols-12 grid-rows-6 h-full gap-8 overflow-y-auto">
+        <div className="col-span-full sm:row-span-2 bg-black rounded-xl">1</div>
+        <div className="col-span-2 row-span-2 md:col-span-3 md:row-span-4 bg-black rounded-xl">
+          2
+        </div>
+        <div className="col-span-2 row-span-3 md:col-span-4 md:row-span-2 lg:col-span-3 bg-black rounded-xl">
+          3
+        </div>
+        <div className="col-span-2 row-span-3 md:row-span-2 md:col-span-5 lg:col-span-4 bg-black rounded-xl">
+          4
+        </div>
+        <div className="col-span-2 row-span-2 md:col-span-6 lg:col-span-2 lg:row-span-4 bg-black rounded-xl">
+          5
+        </div>
+        <div className="col-span-2 row-span-2 md:col-span-6 lg:col-span-5 bg-black rounded-xl hidden lg:block">
+          6
+        </div>
+        <div className="col-span-2 row-span-1 hidden md:col-span-3 md:row-span-2 md:block lg:col-span-2 bg-black rounded-xl lg:block">
+          7
+        </div>
       </div>
     </div>
   );
