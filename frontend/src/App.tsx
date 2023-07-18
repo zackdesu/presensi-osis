8f5e5d61bb7a0c82e81d6d46f61960a1107c70ad
zackdesu <zarchxxx@gmail.com>
2023-07-18 07:48:08 +0700

    route to presensi

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import SideBar from "./components/sidebar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
+import Presensi from "./pages/presensi";
 
 const App = () => {
   return (
@@ -15,8 +16,15 @@ const App = () => {
           }
           path="/"
         />
-        <Route element={<SideBar />} path="/presensi" />
-        <Route element={<SideBar />} path="/cari" />
+        <Route
+          element={
+            <>
+              <SideBar /> <Presensi />{" "}
+            </>
+          }
+          path="/presensi"
+        />
+        <Route element={<SideBar />} path="/galeri" />
         <Route element={<SideBar />} path="/tentang" />
       </Routes>
     </Router>
