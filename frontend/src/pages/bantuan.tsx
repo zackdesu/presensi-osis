f66d701fb5c6a43d7f4115c075a80c17d2b2890e
zackdesu <zarchxxx@gmail.com>
2023-07-18 14:07:12 +0700

    add bantuan page

---
+++ frontend/src/pages/bantuan.tsx
@@ -0,0 +1,14 @@
+import Header from "../components/header";
+
+const bantuan = () => {
+  return (
+    <div className="grid grid-cols-4 md:grid-cols-12 grid-rows-6 h-full p-5 gap-8">
+      <Header />
+      <div className="flex flex-col items-center col-span-full row-span-5">
+        <h3 className="mb-3">Bantuan</h3>
+      </div>
+    </div>
+  );
+};
+
+export default bantuan;
