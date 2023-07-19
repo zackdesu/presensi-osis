f0d592d6439cb3421599520f1a4f1d4a016dae3d
zackdesu <zarchxxx@gmail.com>
2023-07-19 18:33:13 +0700

    shorten agenda code

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,22 +1,17 @@
 import Agenda from "../components/agenda";
 import ProfileCard from "../components/profilecard";
 import Calendar from "../components/calendar";
-import { dataAgenda, dataQuote } from "../components/data";
 import Header from "../components/header";
+import { dataQuote } from "../components/data";
 
 const Home = () => {
   const Random = Math.floor(Math.random() * dataQuote.length);
 
   return (
-    <div className="grid grid-cols-4 md:grid-cols-12 grid-rows-6 h-full p-5 gap-8">
+    <div className="grid grid-cols-4 md:grid-cols-12 grid-rows-6 h-full p-5 gap-8 ml-20">
       <Header />
       <section className="col-span-4 sm:col-span-2 md:col-span-6 lg:col-span-5 row-span-2 md:row-span-3 rounded-xl bg-zinc-950 flex flex-col items-center p-5">
-        <h4 className="mb-4">Agenda</h4>
-        <div className="overflow-y-auto w-full">
-          {dataAgenda.map((a, i) => (
-            <Agenda name={a.name} time={a.date} key={i} />
-          ))}
-        </div>
+        <Agenda />
       </section>
 
       <aside className="hidden sm:flex sm:col-span-2 md:col-span-6 lg:col-span-4 row-span-3 md:col-end-13 md:row-span-3 bg-zinc-950 rounded-xl px-5 py-1 flex-col items-center">
