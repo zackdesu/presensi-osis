c6ebb12babbc9d94dc924ff421d7db6ac753dbfc
zackdesu <zarchxxx@gmail.com>
2023-07-21 16:01:19 +0700

    use return, restyling & add no & have account link page

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,6 +1,6 @@
 import { FormEvent } from "react";
 import api from "../api/axios";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -20,36 +20,50 @@ const Login = () => {
         navigate("/");
       })
       .catch((err) => console.log(err.response.data.message));
+
+    return;
   };
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col items-center m-5">
+    <form onSubmit={handleSubmit} className="flex flex-col items-center p-5">
       <img src="/osis.svg" alt="osissmkn1dumai" width={100} />
-      <h2 className="my-10">OSIS SMK Negeri 1 Dumai</h2>
+      <h2 className="my-10">Masuk ke akunmu</h2>
 
       <div className="flex flex-col">
         <label htmlFor="username" className="font-medium text-zinc-400">
-          Nama
+          <p>Nama</p>
         </label>
         <input
           type="text"
           id="username"
           name="username"
-          className="outline-none border-b bg-transparent p-2 text-lg mb-5"
+          className="outline-none bg-transparent border-b p-3 text-lg mb-5 w-[99%] mx-auto focus:border-orange-300"
+          required
+          autoComplete="off"
         />
         <label htmlFor="password" className="font-medium text-zinc-400">
-          Password
+          <p>Password</p>
         </label>
         <input
           type="password"
           id="password"
           name="password"
-          className="outline-none border-b bg-transparent p-2 text-lg mb-5 font-bold tracking-widest"
+          className="outline-none bg-transparent border-b p-3 text-lg mb-5 w-[99%] mx-auto font-bold tracking-widest focus:border-orange-300 invalid:text-red-600 invalid:focus:border-red-500"
+          required
+          autoComplete="off"
+          minLength={6}
         />
 
         <button className="px-8 py-2 font-semibold bg-orange-500 rounded-lg my-8">
           Login
         </button>
+
+        <p className="text-center">
+          Belum memiliki akun? Klik{" "}
+          <Link to={"/register"} className="text-orange-300">
+            disini
+          </Link>
+        </p>
       </div>
     </form>
   );
