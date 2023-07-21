a1eafa29b50f59c3efa929ff58b5da92b2ad8cfa
zackdesu <zarchxxx@gmail.com>
2023-07-21 15:57:33 +0700

    pathname to lowercase

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -69,7 +69,7 @@ const App = () => {
                 <Register />
               </>
             }
-            path="/Register"
+            path="/register"
           />
         </Routes>
       </HeaderProvider>
