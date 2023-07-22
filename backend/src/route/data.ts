89f03ff4a5b2096c8a6b427192e3e4031436cc23
zackdesu <zarchxxx@gmail.com>
2023-07-22 15:56:09 +0700

    remove datapertemuan route

--- backend/src/route/data.ts
+++ backend/src/route/data.ts
@@ -8,12 +8,6 @@ router.get("/datauser", async (req: Request, res: Response) => {
   res.status(200).json(data);
 });
 
-router.get("/datapertemuan", async (req: Request, res: Response) => {
-  const data: object[] = await prisma.pertemuan.findMany();
-
-  res.status(200).json(data);
-});
-
 router.get("/dataagenda", async (req: Request, res: Response) => {
   const data: object[] = await prisma.agenda.findMany();
 
