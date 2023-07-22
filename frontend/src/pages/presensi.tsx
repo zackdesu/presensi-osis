6de1f321e6aad086855e2b7ae67278562b52f04a
zackdesu <zarchxxx@gmail.com>
2023-07-22 10:15:46 +0700

    presensi setting (halfdone)

--- frontend/src/pages/presensi.tsx
+++ frontend/src/pages/presensi.tsx
@@ -1,15 +1,13 @@
 import Header from "../components/header";
 import { dataUser } from "../components/data";
 import { useEffect, useState } from "react";
-import { DataAgendaPertemuan } from "../components/type";
+import { DataAgendaPertemuan, DataUser } from "../components/type";
 import api from "../api/axios";
 
 const Presensi = () => {
   const [dataPertemuan, setDataPertemuan] = useState<DataAgendaPertemuan[]>([]);
   const [hadir, setHadir] = useState<boolean>(false);
-  const [kehadiranUser, setKehadiranUser] = useState<number>(
-    dataUser[0].kehadiran
-  );
+  const [user, setUser] = useState<DataUser>();
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
@@ -17,9 +15,16 @@ const Presensi = () => {
       .get("/datapertemuan")
       .then((res) => {
         setDataPertemuan(res.data);
-        setIsLoading(false);
       })
       .catch((err) => console.log(err));
+
+    api
+      .get("/login")
+      .then((res) => setUser(res.data))
+      .catch((err) => console.log(err));
+
+    setIsLoading(false);
+
     return;
   }, []);
 
@@ -27,8 +32,6 @@ const Presensi = () => {
     if (!hadir) return;
     dataUser[0].statusHadir = "Hadir";
     console.log(dataUser[0].statusHadir);
-    setKehadiranUser(kehadiranUser + 1);
-    dataUser[0].kehadiran += kehadiranUser;
     console.log(dataUser[0].kehadiran);
   }, [hadir, setHadir]);
 
@@ -40,27 +43,30 @@ const Presensi = () => {
         <h2>
           {isLoading
             ? 0
-            : Math.round((kehadiranUser / dataPertemuan.length) * 100)}
+            : Math.round(
+                (user ? user.kehadiran / dataPertemuan.length : 0) * 100
+              )}
           %
         </h2>
         <p>
           {isLoading
             ? "Loading..."
-            : `${kehadiranUser} dari ${dataPertemuan.length} pertemuan`}
+            : `${user && user.kehadiran} dari ${
+                dataPertemuan.length
+              } pertemuan`}
         </p>
       </section>
       <section className="bg-zinc-950 rounded-xl col-span-full flex items-center justify-center md:col-span-7">
         <img
-          src={dataUser[0].img}
-          alt={dataUser[0].name}
+          src={user && (user.img as string)}
+          alt={user && (user.name as string)}
           className="w-[30px] h-[30px] md:w-[35px] md:h-[35px] rounded-full mr-3"
         />
-        <h6 className="mr-2 md:mr-5">{dataUser[0].name}</h6> |{" "}
-        <p className="ml-2 md:ml-5">{dataUser[0].role}</p>
+        <h6 className="mr-2 md:mr-5">{user && (user.name as string)}</h6> |{" "}
+        <p className="ml-2 md:ml-5">{user && (user.role as string)}</p>
       </section>
       <section className="bg-zinc-950 rounded-xl flex col-span-full md:col-span-7 items-center justify-center">
-        <h6>Status Presensi:&nbsp;</h6>{" "}
-        <p>{hadir ? "Hadir" : "Belum Melakukan Presensi"}</p>
+        <h6>Status Presensi:&nbsp;</h6> <p>{user && user.hadir}</p>
       </section>
       <section className="bg-zinc-950 rounded-xl flex flex-col md:flex-row col-span-full row-span-3 items-center justify-center">
         <div className="md:w-3/4 flex flex-col items-center justify-center">
