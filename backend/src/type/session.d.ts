34a46947152996b2164b52bd832448f92e17ad4f
zackdesu <zarchxxx@gmail.com>
2023-07-29 20:03:23 +0700

    declare session

---
+++ backend/src/type/session.d.ts
@@ -0,0 +1,18 @@
+import { Attendance } from "./attendance";
+
+declare module "express-session" {
+  interface Session {
+    user: {
+      id: string;
+      img: string | null;
+      kehadiran: number;
+      name: string;
+      password: string;
+      role: "Administrator" | "Moderator" | "Member" | string;
+      status: "Online" | "Offline" | string | null;
+      statusHadir?: Attendance[];
+      hadir?: boolean;
+      pertemuanDihadiri?: [{ id: string }];
+    };
+  }
+}
