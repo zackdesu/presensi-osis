f18cdc586686c96c9acc448786f3b637c6362f48
zackdesu <zarchxxx@gmail.com>
2023-08-04 21:39:06 +0700

    delete jansen.jpg

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -20,12 +20,7 @@ const Home = () => {
       <section className="bg-zinc-950 col-span-4 row-span-2 sm:col-span-2 md:col-span-5 md:row-span-2 lg:col-span-3 lg:row-span-5 rounded-xl flex flex-col items-center p-5">
         <ProfileCard />
       </section>
-      <section className="bg-zinc-950 row-span-2 max-sm:hidden sm:col-span-2 sm:row-span-3 md:col-span-7 md:row-span-1 lg:row-span-2 lg:col-span-9 rounded-xl">
-        <img
-          src="/jansen.jpg"
-          className="object-cover rounded-xl object-center w-full h-full"
-        />
-      </section>
+      <section className="bg-zinc-950 row-span-2 max-sm:hidden sm:col-span-2 sm:row-span-3 md:col-span-7 md:row-span-1 lg:row-span-2 lg:col-span-9 rounded-xl"></section>
       <section className="bg-zinc-950 col-span-4 sm:col-span-2 sm:row-span-2 md:col-span-7 md:row-span-1 lg:hidden rounded-xl flex flex-col items-center p-2">
         <h6 className="mb-2 md:mb-0">#QoTD</h6>
         <p className="text-center">"{dataQuote[Random]}"</p>
