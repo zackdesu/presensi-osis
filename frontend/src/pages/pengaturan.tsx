3894e58f00317a21c41f784f2f0415baf3c2ad03
zackdesu <zarchxxx@gmail.com>
2023-08-04 18:36:50 +0700

    edit file name from bantuan to pengaturan

--- frontend/src/pages/pengaturan.tsx
+++ frontend/src/pages/pengaturan.tsx
@@ -1,14 +1,14 @@
 import Header from "../components/header";
 
-const bantuan = () => {
+const Pengaturan = () => {
   return (
     <div className="grid grid-cols-4 md:grid-cols-12 grid-rows-6 h-full p-5 gap-8 ml-20">
       <Header />
       <div className="flex flex-col items-center col-span-full row-span-5">
-        <h3 className="mb-3">Bantuan</h3>
+        <h3 className="mb-3">Pengaturan</h3>
       </div>
     </div>
   );
 };
 
-export default bantuan;
+export default Pengaturan;
