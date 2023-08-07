38d199092c58818c2cee66b3fc25210063c5cb32
zackdesu <zarchxxx@gmail.com>
2023-08-07 16:55:44 +0700

    axios development and production configuration

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,14 @@
 import axios from "axios";
 
+const baseURL =
+  import.meta.env.MODE === "development"
+    ? import.meta.env.VITE_LOCAL_BACKEND_API
+    : import.meta.env.VITE_BACKEND_API;
+
+if (!baseURL) console.error("Environment not Found!");
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_API as string,
+  baseURL,
   headers: {
     "Content-Type": "application/json",
   },
