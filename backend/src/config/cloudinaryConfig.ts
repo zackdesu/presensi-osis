969f2d1b8e7a8862671ff5f12c0b2b97fc6f9330
zackdesu <zarchxxx@gmail.com>
2023-08-11 16:39:59 +0700

    cloudinary config

---
+++ backend/src/config/cloudinaryConfig.ts
@@ -0,0 +1,8 @@
+const cloudinaryConfig = {
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+  secure: true,
+};
+
+export default cloudinaryConfig;
