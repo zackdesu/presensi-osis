27c43dc6480052db732a696ff146271dd347175b
zackdesu <zarchxxx@gmail.com>
2023-08-11 18:02:17 +0700

    edit avatar

--- frontend/src/pages/pengaturan.tsx
+++ frontend/src/pages/pengaturan.tsx
@@ -1,7 +1,10 @@
-import { FormEvent, useContext } from "react";
+import { ChangeEvent, FormEvent, useContext } from "react";
 import Header from "../components/header";
 import { HeaderContext } from "../api/headerContext";
 import { Link } from "react-router-dom";
+import { AiFillCamera } from "react-icons/ai";
+import api from "../api/axios";
+import toast from "react-hot-toast";
 
 const Pengaturan = () => {
   const data = useContext(HeaderContext);
@@ -16,21 +19,58 @@ const Pengaturan = () => {
     console.log(data);
   };
 
+  const handleAvatar = (e: ChangeEvent<HTMLInputElement>) => {
+    if (!e.currentTarget.files) return;
+    const avatar = e.currentTarget.files[0];
+    if (!avatar.type.includes("image")) return console.error("Not an Image!");
+
+    const formData: FormData = new FormData();
+    formData.append("file", avatar);
+
+    api
+      .post("/useravatar", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((res: { data: { message: string } }) => {
+        toast.success(res.data.message);
+      })
+      .catch((err) => console.error(err));
+  };
+
   return (
     <div className="grid grid-cols-4 md:grid-cols-12 grid-rows-6 h-full p-5 gap-8 ml-20">
       <Header />
       <div className="flex flex-col items-center col-span-full row-span-5 overflow-auto">
         <h3 className="mb-3">Pengaturan</h3>
-        <div className="flex justify-around w-full my-5">
+        <div className="flex justify-around w-full my-5 flex-wrap">
           <div>
-            <img
-              src={data.img}
-              className="rounded-full"
-              width={150}
-              height={150}
-            />
+            <div className="relative h-fit">
+              <img
+                src={data.img}
+                alt={data.name}
+                className="rounded-full"
+                width={150}
+                height={150}
+              />
+              <label
+                htmlFor="file"
+                className="absolute right-0 bottom-0 rounded-full bg-white text-black border border-black cursor-pointer p-1"
+              >
+                <AiFillCamera size={20} />
+              </label>
+              <input
+                type="file"
+                name="file"
+                id="file"
+                hidden
+                accept="image/*"
+                onChange={handleAvatar}
+              />
+            </div>
           </div>
-          <form onSubmit={handleSubmit} className="flex flex-col">
+          <form onSubmit={handleSubmit} className="flex flex-col max-sm:mt-8">
             <label htmlFor="username">Name</label>
             <input
               type="text"
@@ -39,6 +79,7 @@ const Pengaturan = () => {
               name="username"
               id="username"
               autoComplete="off"
+              disabled
             />
 
             <Link to="/pengaturan/password">
@@ -49,7 +90,7 @@ const Pengaturan = () => {
               type="submit"
               className="px-8 py-2 font-semibold bg-orange-500 rounded-lg my-8"
             >
-              Simpan perubahan
+              <p>Simpan perubahan</p>
             </button>
           </form>
         </div>
